7c6baacac096bff6f8be5df7150345d50634a831
Zwivhuya Muthathi <88210910+Vhazee@users.noreply.github.com>
2023-07-05 22:48:16 +0200

    A simple program to rate movies

---
+++ MovieRatingSystemDemo.cpp
@@ -0,0 +1,70 @@
+//Movie rating app
+#include <iostream> 
+using namespace std;
+
+//void fuction to display movies
+void displayMovies()
+{
+    cout << "Welcome to the phoenix movie rating system \n";
+    cout << "Choose levels from 1(worse) to 5(best) \n";
+    cout << "Choose from the movies below which one you'd like to rate: "<<endl;
+    cout <<endl<< "1. Spiderman - Far From Home" << endl;
+    cout << "2. Avengers - End game" << endl;
+    cout << "3. Captain America - Civil War" << endl;
+    cout << "4. Murder Mystery 2" << endl;
+    cout << "5. Transformers - Rise Of The Beasts" << endl;
+}
+
+string chooseMovie(int movieNum)
+{
+ string movieSelect[] = {"Spiderman - Far From Home", "Avengers - End game", "Captain America - Civil War",
+ "Murder Mystery 2", "Transformers - Rise Of The Beasts"};
+ 
+ if (movieNum >= 1 && movieNum <= 5)
+    return(movieSelect[movieNum - 1]);
+}
+
+
+string rate(int level)
+{
+    string rated;
+    while(level <= 0)
+    {
+        cout << "Please enter integer numbers from 1-5: ";
+        cin >> level;
+    }
+    if (level == 1 && level <=2)
+    {
+        rated = "Poor";
+    }
+    if(level > 2 && level <= 4)
+    {
+        rated = "Average";
+    }
+    if (level == 5)
+    {
+        rated = "Great";
+    }
+    return rated;
+}
+
+
+int main()
+{
+    int selectMovie;
+    displayMovies();
+    
+    cout <<endl<< "Choose a movie to rate: ";
+    cin >> selectMovie;
+    
+    cout <<chooseMovie(selectMovie);
+    
+    int rating;
+    cout <<endl<< "How do you rate this movie (Choose integers from 1-5): ";
+    cin >> rating;
+    
+    cout <<endl<< "Movie choosen is "<<chooseMovie(selectMovie) << " and is rated ";
+    cout << rate(rating) << endl;
+    
+    return 0;
+}
